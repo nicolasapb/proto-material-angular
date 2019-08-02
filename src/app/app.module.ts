658cbc036468e276dac71cb831b9d131ed57e8b7
nicolas <nicolas.a.p.barbosa@gmail.com>
2019-08-01 22:10:11 -0300

    inicialização angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTableModule }  from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,8 @@ import { SavingsComponent } from './savings/savings.component';
 import { EconomiasComponent } from './economias/economias.component';
 import { ChartsModule } from 'ng2-charts';
 import { ParcelasComponent } from './parcelas/parcelas.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -39,10 +42,17 @@ import { ParcelasComponent } from './parcelas/parcelas.component';
     MatProgressBarModule,
     MatSidenavModule,
     MatTableModule,
-    MatCardModule,    
+    MatCardModule,
     MatButtonModule,
     MatIconModule,
-    ChartsModule
+    ChartsModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
